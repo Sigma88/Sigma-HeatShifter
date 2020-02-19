4af440fe4885ed4eed35bda5d02a806370a80c42
Sigma88 <sigmamodexpansions@outlook.com>
2020-02-19 12:40:11 +0100

    kopernicus is a dependancy

--- [Source]/SigmaHeatShifter/Properties/AssemblyInfo.cs
+++ [Source]/SigmaHeatShifter/Properties/AssemblyInfo.cs
@@ -8,5 +8,6 @@
 [assembly: AssemblyCopyright("Copyright (C) Sigma88 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: KSPAssembly("SigmaHeatShifter", 1, 0)]
 [assembly: AssemblyVersion("1.0.0")]
+[assembly: KSPAssembly("SigmaHeatShifter", 1, 0)]
+[assembly: KSPAssemblyDependency("Kopernicus", 1, 0)]
